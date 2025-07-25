bb05080940ccd164f0cc94e5525a278f945f5bac
NiaDicks <ndicks@clemson.edu>
2025-07-25 12:03:07 -0400

    changed css for styling

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,12 @@
 // imports need to make this app run
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
 
 
 // react wrapper fro pwoer bi js sdk
 import { PowerBIEmbed } from 'powerbi-client-react';
 // power bi constants/enums
-import { models, type IEmbedConfiguration, Embed } from 'powerbi-client';
+import * as powerbi from 'powerbi-client';
 // css styling
 import './App.css'
 
@@ -27,7 +27,9 @@ interface EmbedTokenResponse {
 const App: React.FC = () => {
     const [reports, setReports] = useState<Report[]>([]);
     //power bi configuration
-    const [embedConfig, setEmbedConfig] = useState<IEmbedConfiguration | null>(null);
+    const [embedConfig, setEmbedConfig] = useState<powerbi.IEmbedConfiguration | null>(null);
+    const [embedKey, setEmbedKey] = useState<string>('');
+    const embedRef = useRef<HTMLDivElement | null>(null);
     
     useEffect(() => {
         axios.get<Report[]>('http://localhost:3001/api/reports')
@@ -35,10 +37,13 @@ const App: React.FC = () => {
         .catch(err => console.log(err));
     }, []);
     
-    // function that trigger microsoft login request
+    // function load and embed a report
     const loadReport = async (report: Report) => {
             try {
-               //pass in the access token to the backend to 1) authenticate user 2) generate the embed token need to embed report
+                if (embedRef.current) {
+                    window['powerbi'].reset(embedRef.current);
+                }
+                
                 const res = await axios.post<EmbedTokenResponse>(
                     'http://localhost:3001/api/embed-token',
                     {reportId: report.id}
@@ -46,37 +51,43 @@ const App: React.FC = () => {
                 
                 // backend returns the embedToken, embedUrl, and reportId, all needed in PowerBIEmbed Tag
                 const {embedToken, embedUrl, reportId: id, reportType} = res.data;
+                const typeOfReport = reportType === 'PaginatedReport' ? 'paginatedReport' : 'report';
 
                 // set the configuration need for PowerBIEmbed
                 //settings for embed itself (cna be altered easily)
-                setEmbedConfig({
-                    type: 'report',
-                    id: id,
-                    embedUrl: embedUrl,
-                    accessToken: embedToken,
-                    tokenType: models.TokenType.Embed,
-                    settings: {
-                        panes: {
-                            filters: {visible: false},
-                            pageNavigation: {visible: true},
+                setEmbedConfig(null);
+                console.log(embedConfig);
+                setTimeout(() => {
+                    setEmbedConfig({
+                        type: 'report',
+                        id: id,
+                        embedUrl: embedUrl,
+                        accessToken: embedToken,
+                        tokenType: powerbi.models.TokenType.Embed,
+                        settings: {
+                            panes: {
+                                filters: {visible: false},
+                                pageNavigation: {visible: true},
+                            },
+                            background: powerbi.models.BackgroundType.Transparent,
                         },
-                        background: models.BackgroundType.Transparent,
-                    },
-                });
-            } catch (err) {
-                console.error('Login or embed error:', err);
-            }
+                    });
+                    setEmbedKey(id + typeOfReport);
+                }, 0);
+                } catch (err) {
+                    console.error('Login or embed error:', err);
+                }
     };
 
     /* If embedConfig is set up simply returns the embed report itself
     PowerBIEmbed is wrapper that renders and handles an iframe */
     return (
         <div style={{ display: 'flex', height: '100vh' }}>
-            <div style={{ width: '250px', padding: '1rem', background: '#f4f4f4', overflowY: 'auto' }}>
+            <div style={{ width: '250px', padding: '1rem', background: '#0967d2', overflowY: 'auto' }}>
                 <h3>Available Reports</h3>
                 {reports.map(report => (
                     <div key={report.id} style={{ marginBottom: '1rem' }}>
-                        <button onClick={() => loadReport(report)} style={{ width: '100%' }}>
+                        <button onClick={() => loadReport(report)} style={{ width: '100%', background: '#0967d2' }}>
                             {report.name} 
                         </button>
                     </div>
@@ -85,11 +96,12 @@ const App: React.FC = () => {
             <div style={{ flexGrow: 1 }}>
                 {embedConfig && (
                     <PowerBIEmbed
+                        key={embedKey}
                         embedConfig={embedConfig}
                         cssClassName="report-style-class"
-                        getEmbeddedComponent={(embedObject: Embed) =>
-                            console.log('Report loaded:', embedObject)
-                        }
+                        getEmbeddedComponent={(embedObject: powerbi.Embed) => {
+                            console.log('Report loaded:', embedObject);
+                        }}
                     />
                 )}
             </div>
